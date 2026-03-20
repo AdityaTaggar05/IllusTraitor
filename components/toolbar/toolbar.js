4a8730cca5d5ee69440d55964b1aad7a3b8a75e9
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-21 02:48:15 +0530

    [feat] add tool selection styling

---
+++ components/toolbar/toolbar.js
@@ -0,0 +1,9 @@
+const tools = document.querySelectorAll(".tool");
+tools.forEach((tool) => {
+  tool.addEventListener("click", () => {
+    if (tool.classList.contains("active")) return;
+
+    tools.forEach((t) => t.classList.remove("active"));
+    tool.classList.add("active");
+  });
+});
