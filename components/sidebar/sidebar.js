964238d78c71299d1fddf62d65446d39014db14f
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-22 02:23:48 +0530

    move tooltip update code from components to js directory

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,14 +1,3 @@
 document.querySelector(".sidebar #close").addEventListener("click", () => {
   document.querySelector(".sidebar").classList.remove("active");
 });
-
-//const slider = document.querySelector("input.slider");
-//const tooltip = document.querySelector(".sidebar .option .tooltip");
-
-//function updateTooltip() {
-//const value = slider.value;
-//tooltip.textContent = value;
-//}
-
-//slider.addEventListener("input", updateTooltip);
-//updateTooltip();
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,18 @@
-export function renderSidebar(tool, state) {
+function updateTooltip(slider, tooltip) {
+  const value = slider.value;
+  tooltip.textContent = value;
+}
+
+export function renderSidebar(tool) {
   const content = document.querySelector(".sidebar .content");
   content.innerHTML = tool.getSidebarOptions();
+  tool.bindSidebarEvents();
+
+  const slider = document.querySelector("input[type='range']");
+  if (slider) {
+    const tooltip = document.querySelector(".sidebar .option .tooltip");
+
+    slider.addEventListener("input", () => updateTooltip(slider, tooltip));
+    updateTooltip(slider, tooltip);
+  }
 }
