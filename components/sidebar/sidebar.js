b662e335eff17432366bc8daa08b5cf98fdcf273
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-22 01:51:28 +0530

    fix canvas styling issue and import main.js

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -2,13 +2,13 @@ document.querySelector(".sidebar #close").addEventListener("click", () => {
   document.querySelector(".sidebar").classList.remove("active");
 });
 
-const slider = document.getElementById("slider");
-const tooltip = document.getElementById("tooltip");
+//const slider = document.querySelector("input.slider");
+//const tooltip = document.querySelector(".sidebar .option .tooltip");
 
-function updateTooltip() {
-  const value = slider.value;
-  tooltip.textContent = value;
-}
+//function updateTooltip() {
+//const value = slider.value;
+//tooltip.textContent = value;
+//}
 
-slider.addEventListener("input", updateTooltip);
-updateTooltip();
+//slider.addEventListener("input", updateTooltip);
+//updateTooltip();
